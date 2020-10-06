fadbc8a9102c5c65794b2bab500531b9bf73db90
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-06 18:16:35 +0200

    Dynamic tasktiles in ListView

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:prototype1_todo/models/task.dart';
 import 'package:prototype1_todo/widgets/tasks_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Do pushups'),
+    Task(name: 'Make Breakfast'),
+    Task(name: 'Meal prep'),
+    Task(name: 'Cardio'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            });
+      },
+      itemCount: tasks.length,
     );
   }
 }
